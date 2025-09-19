92febc49a54603fcb011f306e1263499c5284590
SudharshanChejarla <sudharshanchejarla@gmail.com>
2025-09-20 00:49:39 +0530

    Second Commit

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,23 +1,5 @@
 
 
-// import { createContext, useState, useEffect } from "react";
-
-// export const ThemeContext = createContext({});
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-
-//   useEffect(() => {
-//     localStorage.setItem("theme", theme);
-//   }, [theme]);
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, setTheme }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-
 
 import { createContext, useState, useEffect } from "react";
 
